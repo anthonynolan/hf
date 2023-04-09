d6bdb18c52da187c41ef62643f8ffcaa3933bf93
Anthony Nolan <anthonynolan@gmail.com>
2023-04-09 17:22:51 +0100

    new file:   tf_generator.py

---
+++ tf_generator.py
@@ -0,0 +1,7 @@
+from transformers import pipeline
+
+generator = pipeline("text-generation", model="gpt2")
+a = generator("Hello, My name is Anthony and", max_length=300, num_return_sequences=3)
+## [{'generated_text': "Hello, I'm a language modeler. So while writing this, when I went out to meet my wife or come home she told me that my"},
+##  {'generated_text': "Hello, I'm a language modeler. I write and maintain software in Python. I love to code, and that includes coding things that require writing"}, ...
+print(a)
